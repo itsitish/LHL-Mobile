02fba080f3e3f37cb2f9cbe42baccda8d8c12140
Itish <itish.singh1@bp.com>
2021-01-20 14:15:15 +0530

    adding segment code

---
+++ Segment-Trip/src/app/shared/pattern.ts
@@ -0,0 +1,95 @@
+export class Pattern {
+  static Pattern = [{
+    "title": "Static",
+    "code": "M/0",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Blink",
+    "code": "M/1",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Breath",
+    "code": "M/2",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Color Wipe	",
+    "code": "M/3",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Color Wipe Inverse	",
+    "code": "4",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Color Wipe Random	",
+    "code": "M/7",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Random Color	",
+    "code": "M/8",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Multi Dynamic	",
+    "code": "M/10",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Rainbow",
+    "code": "M/11",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Rainbow Cycle	",
+    "code": "M/12",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Dual Scan	",
+    "code": "M/14",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Fade	",
+    "code": "M/15",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Theater Chase Rainbow	",
+    "code": "M/17",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Running Lights	",
+    "code": "M/18",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Multi Strobe	",
+    "code": "M/28",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Strobe",
+    "code": "M/26",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Blink Rainbow	",
+    "code": "M/29",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Chase Random	",
+    "code": "M/32",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Chase Rainbow	",
+    "code": "M/33",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Color Sweep Random	",
+    "code": "M/39",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Running Random	",
+    "code": "M/42",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Comet	",
+    "code": "M/44",
+    'icon': 'heart-outline'
+  }, {
+    "title": "Merry Christmas	",
+    "code": "M/47",
+    'icon': 'heart-outline'
+  }]
+} 
---
+++ Segment-Trip/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'sync',
+        loadChildren: () => import('../multiple-mode/multiple-mode.module').then(m => m.MultipleModePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
