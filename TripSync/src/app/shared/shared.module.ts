1915c1ef30f91531bcfb73186a442dbc37d62ccd
Itish <itish.singh1@bp.com>
2021-04-06 14:05:15 +0530

    pushing prod build changes

--- TripSync/src/app/shared/shared.module.ts
+++ TripSync/src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { EmptyValuePipe } from './pipes/empty-value.pipe';
 import { FavouritesComponent } from './components/favourites/favourites.component'
 import { HeaderComponent } from './components/header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  declarations: [EmptyValuePipe, FavouritesComponent,HeaderComponent],
+  declarations: [EmptyValuePipe, FavouritesComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule, IonicModule
   ],
-  exports: [EmptyValuePipe, FavouritesComponent,HeaderComponent]
+  exports: [EmptyValuePipe, FavouritesComponent, HeaderComponent]
 })
 export class SharedModule { }
